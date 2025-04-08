f82b545d34ddf75160636d8633b653a5982217e6
| Vìv¢k Kumàr Mìshrå | <121348801+Vivek3500@users.noreply.github.com>
2025-04-09 02:03:01 +0530

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Hello, I'm Vivek Kumar Mishra, a 4th year CSE 
+student - proficient in Python, full-stack web 
+development, and C++. I'm currently pursuing 
+my B.Tech in E-Commerce at Vellore Institute 
+of Technology, Bhopal. In my journey, I've already delved into Python, full-stack web development, and C++. I look forward to applying these skills in real-world scenarios and contributing to innovative projects.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
